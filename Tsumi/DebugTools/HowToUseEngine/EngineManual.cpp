1d24cffb062d3ef29e15ee8ceae4daef27a7da85
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-02-04 22:48:02 +0900

    lightはおｋ

--- Tsumi/DebugTools/HowToUseEngine/EngineManual.cpp
+++ Tsumi/DebugTools/HowToUseEngine/EngineManual.cpp
@@ -47,6 +47,8 @@ void EngineManual::Update(ViewProjection view)
 	ImGui::Begin("BallObj");
 	ImGui::ColorEdit4("LightColor", &light_.color.x);
 	ImGui::DragFloat3("LightDirection", &light_.direction.x, 0.01f, -1.0f, 1.0f);
+	ImGui::DragFloat("LightIntensity", &light_.intensity, 0.01f, -1.0f, 1.0f);
+	ImGui::DragFloat("LightSininess", &light_.sininess, 0.1f, 0.0f, 200.0f);
 	ImGui::End();
 
 #endif // _DEBUG

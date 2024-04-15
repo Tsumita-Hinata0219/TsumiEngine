7cf9b6a20d0b0f0d3c192c89d6b7908e70ea24ff
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-04-15 11:21:52 +0900

    assimpをインクルードできた

--- Tsumi/3D/Model/ModelManager/ModelManager.h
+++ Tsumi/3D/Model/ModelManager/ModelManager.h
@@ -8,6 +8,11 @@
 #include <fstream>
 #include <sstream>
 
+#include <assimp\Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp\postprocess.h>
+
+
 /* ModelManagerクラス */
 class ModelManager {
 

bb1cc13a12497fa63cfb971c43cf44f10f88bb6f
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-05 15:47:11 +0900

    マージ時のエラーを修正

--- Project/GameObject/Others/TestAnimationCube/TestAnimationCube.cpp
+++ Project/GameObject/Others/TestAnimationCube/TestAnimationCube.cpp
@@ -10,9 +10,8 @@ void TestAnimationCube::Init()
 	testAnimation_ = AnimationManager::LoadAnimationFile("AnimatedCube", "AnimatedCube");
 
 	wt_.Initialize();
-	wt_.translate.y = 2.0f;
-	wt_.translate.z = 10.0f;
-
+	wt_.srt.translate.y = 2.0f;
+	wt_.srt.translate.z = 10.0f;
 }
 
 

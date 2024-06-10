c573240f502cc27ec84ed34ff20192f197129bce
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-10 10:42:26 +0900

    他ブランチで作ったものをマージ

--- Project/GameObject/Effect/TestPostEffect/TestPostEffect.cpp
+++ Project/GameObject/Effect/TestPostEffect/TestPostEffect.cpp
@@ -7,7 +7,7 @@ void TestPostEffect::Initialize()
 {
 	// リソースなどを作成
 	Create();
-	material_.mtlData.type = int(PostEffectType::GaussianFilter);
+	material_.mtlData.type = int(PostEffectType::Smoothing);
 }
 
 

211d07dc4b5e7c1f12dfab44d2ed74f4c3243442
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-10 09:55:44 +0900

    Smoothingできていなかったから作った

--- Resources/shaders/PostEffect.PS.hlsl
+++ Resources/shaders/PostEffect.PS.hlsl
@@ -23,12 +23,28 @@ static const float2 kINdex3x3[3][3] =
     { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
     { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
 };
+static const float2 kINdex5x5[5][5] =
+{
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
+    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
+    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } }
+};
 // カーネル
 static const float kKernel3x3[3][3] =
 {
     { { 1.0f / 9.0f }, { 1.0f / 9.0f }, { 1.0f / 9.0f } },
     { { 1.0f / 9.0f }, { 1.0f / 9.0f }, { 1.0f / 9.0f } },
     { { 1.0f / 9.0f }, { 1.0f / 9.0f }, { 1.0f / 9.0f } },
+};
+static const float kKernel5x5[5][5] =
+{
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f }
 };
 
 
@@ -74,6 +90,28 @@ PixelShaderOutput main(VertexShaderOutput input)
     }
     else if (gMaterial.type == 4) // Smoothing
     {
+        //// 1. uvStepSizeの算出
+        //uint width, height;
+        //gTexture.GetDimensions(width, height);
+        //float2 uvStepSize = float2(rcp((float) width), rcp((float) height));
+        
+        //output.color.rgb = float3(0.0f, 0.0f, 0.0f);
+        //output.color.a = 1.0f;
+        
+        //// 2. 3x3ループ
+        //for (uint x = 0; x < 3; ++x)
+        //{
+        //    for (uint y = 0; y < 3; ++y)
+        //    {
+        //        // 3. 現在のtexcoordを算出
+        //        float2 texcoord = input.texcoord + kINdex3x3[x][y] * uvStepSize;
+                
+        //        // 4. 色に 1/9 掛けて足す
+        //        float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+        //        output.color.rgb += fetchColor * kKernel3x3[x][y];
+        //    }
+        //}
+        
         // 1. uvStepSizeの算出
         uint width, height;
         gTexture.GetDimensions(width, height);
@@ -82,17 +120,17 @@ PixelShaderOutput main(VertexShaderOutput input)
         output.color.rgb = float3(0.0f, 0.0f, 0.0f);
         output.color.a = 1.0f;
         
-        // 2. 3x3ループ
-        for (uint x = 0; x < 3; ++x)
+        // 2. 5x5ループ
+        for (uint x = 0; x < 5; ++x)
         {
-            for (uint y = 0; y < 3; ++y)
+            for (uint y = 0; y < 5; ++y)
             {
                 // 3. 現在のtexcoordを算出
-                float2 texcoord = input.texcoord + kINdex3x3[x][y] * uvStepSize;
+                float2 texcoord = input.texcoord + kINdex5x5[x][y] * uvStepSize;
                 
-                // 4. 色に 1/9 掛けて足す
+                // 4. 色に 1/25 掛けて足す
                 float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-                output.color.rgb += fetchColor * kKernel3x3[x][y];
+                output.color.rgb += fetchColor * kKernel5x5[x][y];
             }
         }
     }

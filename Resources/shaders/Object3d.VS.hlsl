387fe0b6a90a3309820d85f727fa93aa03dee9c8
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-10 11:58:10 +0900

    わからん

--- Resources/shaders/Object3d.VS.hlsl
+++ Resources/shaders/Object3d.VS.hlsl
@@ -19,7 +19,22 @@ struct VertexShaderInput
 
 
 VertexShaderOutput main(VertexShaderInput input)
-{
+{    
+    //VertexShaderOutput output;
+    //float4x4 resultMat;
+
+    //float4x4 cameraMat = mul(gTransformationViewMatrix.view, gTransformationViewMatrix.viewProjection);
+    //resultMat = mul(gTransformationMatrix.World, cameraMat);
+
+    //output.position = mul(input.position, resultMat);
+    //output.texcoord = input.texcoord;
+    //output.normal = normalize(mul(input.normal, (float3x3) gTransformationMatrix.World));
+    //output.worldPosition = mul(input.position, gTransformationMatrix.World).xyz;
+    //output.cameraPosition = gTransformationViewMatrix.cameraPosition;
+    
+    //return output;
+    
+    
     VertexShaderOutput output;
     
     output.position = mul(input.position, gTransformationMat.WVP);

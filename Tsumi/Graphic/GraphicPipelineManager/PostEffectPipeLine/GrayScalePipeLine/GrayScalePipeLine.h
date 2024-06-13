c755d2b58d1153731943e6e219bc197fe88848cb
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-13 11:16:49 +0900

    GrayScale

--- Tsumi/Graphic/GraphicPipelineManager/PostEffectPipeLine/GrayScalePipeLine/GrayScalePipeLine.h
+++ Tsumi/Graphic/GraphicPipelineManager/PostEffectPipeLine/GrayScalePipeLine/GrayScalePipeLine.h
@@ -1,5 +1,17 @@
 #pragma once
-class GrayScalePipeLine
-{
+
+#include "IPipeLineState.h"
+
+/* BoxFilterPipeLineクラス */
+class GrayScalePipeLine : public IPipeLineState {
+
+public:
+
+	// Psoを構築する
+	void SetupLightPso() override;
+
+	// RootSignatureのセットアップ
+	void SetUpRootSignature(D3D12_ROOT_SIGNATURE_DESC& descriptionRootSignature) override;
+
 };
 

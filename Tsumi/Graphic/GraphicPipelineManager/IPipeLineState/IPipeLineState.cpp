fd99a77e49dc806457d71c193706644e4a330194
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-13 10:39:34 +0900

    PipeLineの基底クラス作成

--- Tsumi/Graphic/GraphicPipelineManager/IPipeLineState/IPipeLineState.cpp
+++ Tsumi/Graphic/GraphicPipelineManager/IPipeLineState/IPipeLineState.cpp
@@ -1,1 +1,108 @@
 #include "IPipeLineState.h"
+
+
+
+// InputLayoutのセットアップ
+void IPipeLineState::SetupInputElementDescs(D3D12_INPUT_ELEMENT_DESC& inputElementDescs, LPCSTR SemanticName, UINT SemanticIndex, DXGI_FORMAT Format, UINT AlignedByteOffset)
+{
+	inputElementDescs.SemanticName = SemanticName;
+	inputElementDescs.SemanticIndex = SemanticIndex;
+	inputElementDescs.Format = Format;
+	inputElementDescs.AlignedByteOffset = AlignedByteOffset;
+}
+void IPipeLineState::SetupInputLayout(D3D12_INPUT_LAYOUT_DESC& inputLayoutDesc, const D3D12_INPUT_ELEMENT_DESC* inputElementDescs, UINT numInputElements)
+{
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = numInputElements;
+}
+
+
+// BlendStateのセットアップ
+void IPipeLineState::SetUpBlendState(D3D12_RENDER_TARGET_BLEND_DESC& blendDesc, BlendMode blendMode)
+{
+	blendDesc.RenderTargetWriteMask =
+		D3D12_COLOR_WRITE_ENABLE_ALL;
+	blendDesc.BlendEnable = true;
+
+
+	switch (blendMode)
+	{
+	case BlendNone:
+		blendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+		blendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
+		blendDesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+		break;
+
+	case BlendAdd:
+		blendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+		blendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
+		blendDesc.DestBlend = D3D12_BLEND_ONE;
+		break;
+
+	case BlendSubtruct:
+		blendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+		blendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT;
+		blendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
+		blendDesc.DestBlend = D3D12_BLEND_ONE;
+		break;
+
+	case BlendMultiply:
+		blendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+		blendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlend = D3D12_BLEND_ZERO;
+		blendDesc.DestBlend = D3D12_BLEND_SRC_COLOR;
+		break;
+
+	case BlendScreen:
+		blendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+		blendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.SrcBlend = D3D12_BLEND_INV_DEST_COLOR;
+		blendDesc.DestBlend = D3D12_BLEND_ONE;
+		break;
+
+	default:
+		break;
+	}
+}
+
+
+// RasterizerStateのセットアップ
+void IPipeLineState::SetUpRasterizerState(D3D12_RASTERIZER_DESC& rasterizerDesc, D3D12_CULL_MODE Cull, D3D12_FILL_MODE fill)
+{
+	// 裏面(時計回り)を表示しない
+	rasterizerDesc.CullMode = Cull;
+	// 三角形の中を塗りつぶす
+	rasterizerDesc.FillMode = fill;
+}
+
+
+// Shadersのコンパイル
+void IPipeLineState::SetUpShader(IDxcBlob*& vertexShaderBlob, IDxcBlob*& pixelShaderBlob, ModelShaders type)
+{
+	vertexShaderBlob = ShaderManager::GetInstance()->GetShader(type).VertexBlob;
+	assert(vertexShaderBlob != nullptr);
+
+	pixelShaderBlob = ShaderManager::GetInstance()->GetShader(type).PixelBlob;
+	assert(pixelShaderBlob != nullptr);
+}
+void IPipeLineState::SetUpShader(IDxcBlob*& vertexShaderBlob, IDxcBlob*& pixelShaderBlob, PostEffectShaders type)
+{
+	vertexShaderBlob = ShaderManager::GetInstance()->GetShader(type).VertexBlob;
+	assert(vertexShaderBlob != nullptr);
+
+	pixelShaderBlob = ShaderManager::GetInstance()->GetShader(type).PixelBlob;
+	assert(pixelShaderBlob != nullptr);
+}
+
--- Tsumi/Graphic/GraphicPipelineManager/IPipeLineState/IPipeLineState.h
+++ Tsumi/Graphic/GraphicPipelineManager/IPipeLineState/IPipeLineState.h
@@ -18,12 +18,32 @@ public: // メンバ関数
 	// Psoを構築する
 	virtual void SetupLightPso() = 0;
 
+	// RootSignatureのセットアップ
+	virtual void SetUpRootSignature(D3D12_ROOT_SIGNATURE_DESC& descriptionRootSignature) = 0;
+
+#pragma region Accessor アクセッサ
+
 	// Psoの取得
 	PsoProperty GetPsoProperty() const { return pso_; }
+
+#pragma endregion
 
 
 protected: // メンバ関数
 
+	// InputLayoutのセットアップ
+	void SetupInputElementDescs(D3D12_INPUT_ELEMENT_DESC& inputElementDescs, LPCSTR SemanticName, UINT SemanticIndex, DXGI_FORMAT Format, UINT AlignedByteOffset);
+	void SetupInputLayout(D3D12_INPUT_LAYOUT_DESC& inputLayoutDesc, const D3D12_INPUT_ELEMENT_DESC* inputElementDescs, UINT numInputElements);
+
+	// BlendStateのセットアップ
+	void SetUpBlendState(D3D12_RENDER_TARGET_BLEND_DESC& blendDesc, BlendMode blendMode);
+
+	// RasterizerStateのセットアップ
+	void SetUpRasterizerState(D3D12_RASTERIZER_DESC& rasterizerDesc, D3D12_CULL_MODE Cull, D3D12_FILL_MODE fill);
+
+	// Shadersのコンパイル
+	void SetUpShader(IDxcBlob*& vertexShaderBlob, IDxcBlob*& pixelShaderBlob, ModelShaders type);
+	void SetUpShader(IDxcBlob*& vertexShaderBlob, IDxcBlob*& pixelShaderBlob, PostEffectShaders type);
 
 protected: // メンバ変数
 

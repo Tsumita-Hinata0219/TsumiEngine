61635bf37fcccd2b30891c38a20b33755ae7d79f
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-13 12:12:26 +0900

    PhongNormalMapPipeLineもできた

--- Tsumi/Graphic/GraphicPipelineManager/ModelPipeLine/PhongNormalMapPipeLine/PhongNormalMapPipeLine.cpp
+++ Tsumi/Graphic/GraphicPipelineManager/ModelPipeLine/PhongNormalMapPipeLine/PhongNormalMapPipeLine.cpp
@@ -104,24 +104,24 @@ void PhongNormalMapPipeLine::SetUpRootSignature(D3D12_ROOT_SIGNATURE_DESC& descr
 
 
 	// 色に関する
-	D3D12_ROOT_PARAMETER rootParameters[5]{};
+	D3D12_ROOT_PARAMETER rootParameters[6]{};
 	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
 	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
 	rootParameters[0].Descriptor.ShaderRegister = 0; // レジスタ番号0とバインド
 
 
 	// 頂点位置に関する
-	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CSVで使う
-	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;//VertexShaderで使う
-	rootParameters[1].Descriptor.ShaderRegister = 0; // レジスタ番号を0にバインド
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;// VertexShaderで使う
+	rootParameters[1].Descriptor.ShaderRegister = 0;// レジスタ番号を0にバインド
 
 	descriptionRootSignature.pParameters = rootParameters; // ルートパラメータ配列へのポインタ
 	descriptionRootSignature.NumParameters = _countof(rootParameters); // 配列の長さ
 
 
 	// Viewに関する
-	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
-	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX; // VertexShaderで使う
+	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 	rootParameters[2].Descriptor.ShaderRegister = 1;
 
 
@@ -137,10 +137,22 @@ void PhongNormalMapPipeLine::SetUpRootSignature(D3D12_ROOT_SIGNATURE_DESC& descr
 	rootParameters[3].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange); // Tableで利用する
 
 
-	// マテリアルに関する
+	// 光に関する
 	rootParameters[4].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
 	rootParameters[4].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
 	rootParameters[4].Descriptor.ShaderRegister = 1; // レジスタ番号0とバインド
+
+
+	// テクスチャに関する
+	D3D12_DESCRIPTOR_RANGE descriptorRangeA[1]{};
+	descriptorRangeA[0].BaseShaderRegister = 1; // 0から始まる
+	descriptorRangeA[0].NumDescriptors = 1; // 数は1つ
+	descriptorRangeA[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND; // offsetを自動計算
+
+	rootParameters[5].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; // DescriptorTableを使う
+	rootParameters[5].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixShaderで使う
+	rootParameters[5].DescriptorTable.pDescriptorRanges = descriptorRangeA; // Tableの中身の配列を指定
+	rootParameters[5].DescriptorTable.NumDescriptorRanges = _countof(descriptorRangeA); // Tableで利用する
 
 
 	// Samplerの設定

d66dae084d638e2d94548c4efa9444531c5d83f9
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-19 22:30:47 +0900

    PipeLineも追加。これでちゃんとけいしょうできればな　

---
+++ Tsumi/Graphic/GraphicPipelineManager/PostEffectPipeLine/ColorGradingPipeLine/ColorGradingPipeLine.h
@@ -0,0 +1,22 @@
+//#pragma once
+//
+//#include "../../IPipeLineState/IPipeLineState.h"
+//
+//
+//
+///* ColorGradingPipeLineクラス */
+//class ColorGradingPipeLine : public IPipeLineState {
+//
+//public:
+//
+//	// Psoを構築する
+//	PsoProperty SetUpPso() override;
+//
+//
+//private:
+//
+//	// RootSignatureのセットアップ
+//	void SetUpRootSignature(D3D12_ROOT_SIGNATURE_DESC& descriptionRootSignature) override;
+//
+//};
+//

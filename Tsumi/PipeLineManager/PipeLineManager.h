852f7c0684bb84803edbb098eff6680e17bd9dac
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-23 13:44:22 +0900

    新しくPipeLineManagerを作成

--- Tsumi/PipeLineManager/PipeLineManager.h
+++ Tsumi/PipeLineManager/PipeLineManager.h
@@ -1,5 +1,145 @@
 #pragma once
-class PipeLineManager
-{
+
+
+#include <d3dx12.h>
+#include <dxcapi.h>
+
+#include "../../../Project/Math/MyMath.h"
+#include "../../../Project/Math/Struct.h"
+#include "../Base/DXCommon/DirectXCommon.h"
+#include "../ShaderManager/ShaderManager.h"
+#include "../CommandManager/CommandManager.h"
+
+#include "IPipeLineState/IPipeLineState.h"
+
+#include "ModelPipeLine/LambertPipeLine/LambertPipeLine.h"
+#include "ModelPipeLine/LightPipeLine/LightPipeLine.h"
+#include "ModelPipeLine/LinePipeLine/LinePipeLine.h"
+#include "ModelPipeLine/NormalPipeLine/NormalPipeLine.h"
+#include "ModelPipeLine/Object3DPipeLine/Object3DPipeLine.h"
+#include "ModelPipeLine/ParticlePipeLine/ParticlePipeLine.h"
+#include "ModelPipeLine/PhongNormalMapPipeLine/PhongNormalMapPipeLine.h"
+#include "ModelPipeLine/PhongPipeLine/PhongPipeLine.h"
+#include "ModelPipeLine/SpritePipeLine/SpritePipeLine.h"
+
+#include "PostEffectPipeLine/BoxFilterPipeLine/BoxFilterPipeLine.h"
+#include "PostEffectPipeLine/DissolvePipeLine/DissolvePipeLine.h"
+#include "PostEffectPipeLine/GaussianFilterPipeLine/GaussianFilterPipeLine.h"
+#include "PostEffectPipeLine/GrayScalePipeLine/GrayScalePipeLine.h"
+#include "PostEffectPipeLine/OutLinePipeLine/OutLinePipeLine.h"
+#include "PostEffectPipeLine/RadialBlurPipeLine/RadialBlurPipeLine.h"
+#include "PostEffectPipeLine/RandomPipeLine/RandomPipeLine.h"
+#include "PostEffectPipeLine/SepiaTonePipeLine/SepiaTonePipeLine.h"
+#include "PostEffectPipeLine/VignettingPipeLine/VignettingPipeLine.h"
+
+
+// PipeLineTypeのenum構造体
+enum class PipeLineType : uint32_t {
+	None,
+	Lambert,
+	Light,
+	Line,
+	Normal,
+	Object3D,
+	Particle,
+	PhongNormalMap,
+	Phong,
+	Sprite,
+	BoxFilter,
+	Dissolve,
+	GaussianFilter,
+	GrayScale,
+	OutLine,
+	RadialBlur,
+	Randome,
+	SepiaTone,
+	Vignetting,
+};
+
+
+/* PipeLineManagerクラス */
+class PipeLineManager {
+
+public: // メンバ関数
+
+	// インスタンスの取得
+	static PipeLineManager* GetInstance() {
+		static PipeLineManager instance;
+		return &instance;
+	}
+
+	// 初期化処理
+	static void Initialize();
+
+
+#pragma region Accessor アクセッサ
+
+	// PipeLineの取得
+	PsoProperty GetPipeLine(PipeLineType type) {
+		return pipeLineMap_[type];
+	}
+
+#pragma endregion
+
+
+private: // メンバ変数
+
+	// PipeLineを保存しておくmapコンテナ
+	std::map<PipeLineType, PsoProperty> pipeLineMap_;
+
+	// Lambert
+	std::unique_ptr<LambertPipeLine> lambertPipeLine_;
+
+	// Light
+	std::unique_ptr<LightPipeLine> lightPipeLine_;
+
+	// Line
+	std::unique_ptr<LinePipeLine> linePipeLine_;
+
+	// Normal
+	std::unique_ptr<NormalPipeLine> normalPipeLine_;
+
+	// Object3D
+	std::unique_ptr<Object3DPipeLine> object3DPipeLine_;
+
+	// Particle
+	std::unique_ptr<ParticlePipeLine> particlePipeLine_;
+
+	// PhongNormalMap
+	std::unique_ptr<PhongNormalMapPipeLine> phongNormalMapPipeLine_;
+
+	// Phong
+	std::unique_ptr<PhongPipeLine> phongPipeLine_;
+
+	// Sprite
+	std::unique_ptr<SpritePipeLine> spritePipeLine_;
+
+	// BoxFilter
+	std::unique_ptr<BoxFilterPipeLine> boxFilterPipeLine_;
+
+	// Dissolve
+	std::unique_ptr<DissolvePipeLine> dissolvePipeLine_;
+
+	// GaussianFilter
+	std::unique_ptr<GaussianFilterPipeLine> gaussianFilterPipeLine_;
+
+	// GrayScale
+	std::unique_ptr<GrayScalePipeLine> grayScalePipeLine_;
+
+	// OutLine
+	std::unique_ptr<OutLinePipeLine> outLinePipeLine_;
+
+	// RadialBlur
+	std::unique_ptr<RadialBlurPipeLine> radialBlurPipeLine_;
+
+	// Random
+	std::unique_ptr<RandomPipeLine> randomPipeLine_;
+
+	// SepiaTone
+	std::unique_ptr<SepiaTonePipeLine> sepiaTonePipeLine_;
+
+	// Vignetting
+	std::unique_ptr<VignettingPipeLine> vignettingPipeLine_;
+
 };
 

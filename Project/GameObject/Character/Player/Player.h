dba36d8df80afb6b2cf81deb62cd560c6e445ded
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-06-26 16:38:09 +0900

    Player.hコピペ。エラーなし

--- Project/GameObject/Character/Player/Player.h
+++ Project/GameObject/Character/Player/Player.h
@@ -1,5 +1,67 @@
 #pragma once
-class Player
-{
+
+#include <list>
+#include <memory>
+
+#include "GameObject.h"
+
+//#include "Bullet/PlayerBullet.h"
+
+
+/* Playerクラス */
+class Player {
+
+public:
+
+	// コンストラクタとデストラクタ
+	Player() {};
+	~Player() {};
+
+	// 初期化処理　更新処理　描画処理
+	void Initialize();
+	void Update();
+	void Draw2DBack(Camera* camera);
+	void Draw3D(Camera* camera);
+	void Draw2DFront(Camera* camera);
+
+private:
+
+	// 移動処理
+	void Move();
+
+	// プレイヤー本体の姿勢処理
+	void CalcBodyRotate();
+
+	// 射撃処理
+	void ExecuteShot();
+
+	// 新しいバレットを生成する
+	void CreateNewBullet();
+
+	// バレットリストの追加
+	//void AddBulletList(std::shared_ptr<PlayerBullet> addBullet) { this->bulletList_.push_back(addBullet); }
+
+
+private:
+
+	// Inputクラス
+	Input* input_ = nullptr;
+
+	// 本体モデル
+	unique_ptr<Model> bodyModel_;
+
+	// 本体座標
+	WorldTransform bodyWt_{};
+
+	// 移動速度
+	Vector3 velocity_{};
+	float moveVector_ = 0.3f;
+
+	// BulletのList配列
+	//std::list<std::shared_ptr<PlayerBullet>> bulletList_;
+
+	// 射撃ボタン押下フレーム
+	int shotPressFrame_ = 0;
+
 };
 

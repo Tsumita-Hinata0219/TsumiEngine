908c816a878d9dbe0427ce66920f997790bbde79
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-07-02 12:48:09 +0900

    Shader作成

---
+++ Resources/Shaders/Skybox/Skybox.VS.hlsl
@@ -0,0 +1,24 @@
+#include "Skybox.hlsli"
+
+struct TransformationMatrix
+{
+    float4x4 WVP;
+};
+ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
+
+struct VertexShaderInput
+{
+    float4 position : POSITIONT0;
+    float3 texcorrd : TEXCOORD0;
+};
+
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    
+    output.position = mul(input.position, gTransformationMatrix.WVP).xyww;
+    output.texcoord = input.position.xyz;
+    
+    return output;
+}
---
+++ Resources/Shaders/Skybox/Skybox.hlsli
@@ -0,0 +1,5 @@
+struct VertexShaderOutput
+{
+    float4 position : SV_POSTION;
+    float3 texcoord : TEXCOORD;
+};

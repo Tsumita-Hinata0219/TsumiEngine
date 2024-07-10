d13581ffca7ddd0a28410265d7cbfd2e46b31c82
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-07-10 14:20:06 +0900

    ステートの基底クラス作成

---
+++ Project/GameObject/Character/EnemyManager/IState/IEnemyState.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+namespace EnemyState {
+
+	enum EnemyState {
+		SPAWN,
+		APPROACH,
+		DEATH,
+	};
+}
+
+/* IEnemyState基底クラス */
+class IEnemyState {
+
+public:
+
+	// コンストラクタ、デストラクタ
+	~IEnemyState() {};
+
+	// 初期化、更新、終了
+	virtual void InitState() = 0;
+	virtual void UpdateState() = 0;
+	virtual void ExitState() = 0;
+
+	// ステート変更
+	virtual void ChangeState(int nextState) = 0;
+
+#pragma region Accessor
+
+	// ステート番号の取得
+	int GetStateNo();
+
+#pragma endregion 
+
+protected:
+
+	static int stateNo_;
+};
+
+

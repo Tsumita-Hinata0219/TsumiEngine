69c1730d4dde22164ff19b0e4b2d5ab1d1901543
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-07-12 22:35:01 +0900

    新しいクラス編集

--- Tsumi/JsonManager/JsonManager.h
+++ Tsumi/JsonManager/JsonManager.h
@@ -1,5 +1,59 @@
 #pragma once
-class JsonManager
-{
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <cmath>
+#include <numbers>
+
+
+#include "json.hpp"
+#include "../../Project/Math/MyMath.h"
+#include "../../Project/GameObject/GameObject.h"
+
+
+/* JsonManagerクラス */
+class JsonManager {
+
+private: // シングルトンデザインパターン
+
+	// コンストラクタ、デストラクタ
+	JsonManager() = default;
+	~JsonManager() = default;
+	JsonManager(const JsonManager&) = delete;
+	const JsonManager& operator=(const JsonManager&) = delete;
+
+
+public: // メンバ関数
+
+	// インスタンスの取得
+	static JsonManager* GetInstance() {
+		static JsonManager instance;
+		return &instance;
+	}
+
+	// 初期化処理、解放処理
+	void Initialize();
+	void Finalize();
+
+	// Jsonファイルの読み込み
+	void LoadJsonFile(const std::string& path, const std::string& fileName);
+
+
+#pragma region Accessor アクセッサ
+
+
+#pragma endregion 
+
+
+private:
+
+
+
+private: // メンバ変数
+
+
+
 };
 

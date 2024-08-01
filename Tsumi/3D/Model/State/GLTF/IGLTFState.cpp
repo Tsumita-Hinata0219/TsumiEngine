b6e8f5fa03bf56cae27836ab8006b276ecc4b8c8
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-08-01 12:07:26 +0900

    派生クラス作成

--- Tsumi/3D/Model/State/GLTF/IGLTFState.cpp
+++ Tsumi/3D/Model/State/GLTF/IGLTFState.cpp
@@ -1,1 +1,14 @@
 #include "IGLTFState.h"
+
+
+
+// 描画処理
+void IGLTFState::Draw()
+{
+}
+
+
+// コマンドコール
+void IGLTFState::CommandCall()
+{
+}
--- Tsumi/3D/Model/State/OBJ/IOBJState.cpp
+++ Tsumi/3D/Model/State/OBJ/IOBJState.cpp
@@ -1,1 +1,14 @@
 #include "IOBJState.h"
+
+
+
+// 描画処理
+void IOBJState::Draw()
+{
+}
+
+
+// コマンドコール
+void IOBJState::CommandCall()
+{
+}

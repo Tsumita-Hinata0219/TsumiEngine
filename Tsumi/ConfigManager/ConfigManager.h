e6c9f53ea4e800bdaaddcd564945bf64fe473def
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-08-03 00:01:50 +0900

    クラス作成

---
+++ Tsumi/ConfigManager/ConfigManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+
+/* 調整項目クラス */
+class ConfigManager {
+
+private: // シングルトンデザインパターン
+
+	// コンストラクタ、デストラクタ
+	ConfigManager() = default;
+	~ConfigManager() = default;
+	ConfigManager(const ConfigManager&) = delete;
+	const ConfigManager& operator=(const ConfigManager&) = delete;
+
+
+
+public:
+
+
+#pragma region Accessor アクセッサ
+
+
+
+
+#pragma endregion 
+
+private:
+
+
+
+
+};
+

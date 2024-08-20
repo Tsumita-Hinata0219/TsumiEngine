9d4b7e5955d4f238aa7fd6517466fd111ed74f32
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-08-21 01:50:44 +0900

    DDSコンバータで作ったDDSでSkyBox表示できた

--- Project/GameObject/Terrain/Skybox/Skybox.cpp
+++ Project/GameObject/Terrain/Skybox/Skybox.cpp
@@ -6,10 +6,10 @@
 void Skybox::Init()
 {
 	// テクスチャの読み込み
-	this->texture_ = TextureManager::LoadTexture("Texture", "airport_4k.dds");
+	this->texture_ = TextureManager::LoadTexture("Texture", "CubeMap.dds");
 
 	// Scale
-	initScale_ = 100.0f;
+	initScale_ = 1000.0f;
 
 	// Transformの初期化処理
 	transform_.Initialize();

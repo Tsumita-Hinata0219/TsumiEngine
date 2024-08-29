25683eebd50e3feb1d3bee1c6a6d47fbc4aab715
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-08-30 02:14:39 +0900

    save

--- Project/Math/MyMath.h
+++ Project/Math/MyMath.h
@@ -6,10 +6,11 @@
 #include "MathOperations/MathOperations.h"
 #include "MathFunction/MathFunction.h"
 #include "MyParlinNoise/ParlinNoise.h"
-#include "../../Tsumi/Utilities/RandomGenerator/RandomGenerator.h"
-#include "../../Tsumi/Utilities/TimedCall/TimedCall.h"
 #include "MathEasing/MathEasing.h"
 
+#include "../../Tsumi/Utilities/RandomGenerator/RandomGenerator.h"
+#include "../../Tsumi/Utilities/TimedCall/TimedCall.h"
+#include "../../Tsumi/SmartPointer/SmartPtr.h"
 
 #define _USE_MATH_DEFINES
 #include <math.h>
--- Tsumi/CollisionSystem/SpatialPartition/Tree/QuadTree/Object/QuadTreeObject.h
+++ Tsumi/CollisionSystem/SpatialPartition/Tree/QuadTree/Object/QuadTreeObject.h
@@ -19,58 +19,100 @@ namespace QTree {
 
 	public:
 
-		// 登録空間
-		QuadTreeSpace<T>* p_Space;
-		// 判定対象オブジェクト
-		T* p_Object;
-		// 前のオブジェクト
-		std::weak_ptr<QuadTreeObject<T>> sp_Pre;
-		// 次のオブジェクト
-		std::shared_ptr<QuadTreeObject<T>> sp_Next;
+		// コンストラクタ
+		QuadTreeObject();
 
-		QuadTreeObject<T>* ok;
+		// デストラクタ
+		~QuadTreeObject();
+
+		// リストから外れる
+		bool Remove();
+
+		// 空間を登録
+		void RegisterSpace(QuadTreeSpace<T>* pSpace);
+
+		// 次のオブジェクトを取得
+		std::shared_ptr<QuadTreeObject<T>> GetNextObj() const;
+
 
 	public:
 
-		// コンストラクタ
-		QuadTreeObject()
-		{
-			// nullを入れておく
-			p_Space = nullptr;
-			p_Object = nullptr;
-		}
+		// 登録空間
+		QuadTreeSpace<T>* p_Space;
 
-		// デストラクタ
-		~QuadTreeObject() {};
+		// 判定対象オブジェクト
+		T* p_Object;
 
-		// リストから外れる
-		bool Remove()
-		{
-			// 既に逸脱しているときは処理終了
-			if (!p_Space) { return false; }
+		// 前のオブジェクト
+		std::shared_ptr<QuadTreeObject<T>> sp_Pre;
 
-			// 自分を登録している空間に自身を通知
-			if (!p_Space->OnRemove(this)) { return false; }
+		// 次のオブジェクト
+		std::shared_ptr<QuadTreeObject<T>> sp_Next;
 
-			// 逸脱処理
-			// 前後のオブジェクトを結びつける
-			if (sp_Pre != nullptr) 
-			{
-
-			}
-		}
-
-		// 空間を登録
-		void RegisterSpace(QuadTreeSpace<T>* pSpace)
-		{
-			p_Space = pSpace;
-		}
-
-		// 次のオブジェクトを取得
-		std::shared_ptr<QuadTreeObject<T>> GetNextObj() const
-		{
-			return sp_Next;
-		}
 	};
+
+
+
+
+
+	// コンストラクタ
+	template<class T>
+	inline QuadTreeObject<T>::QuadTreeObject()
+	{
+		// nullを入れておく
+		p_Space = nullptr;
+		p_Object = nullptr;
+	}
+
+
+	// デストラクタ
+	template<class T>
+	inline QuadTreeObject<T>::~QuadTreeObject()
+	{
+	}
+
+
+	// リストから外れる
+	template<class T>
+	inline bool QuadTreeObject<T>::Remove()
+	{
+		// 既に逸脱しているときは処理終了
+		if (!p_Space) { return false; }
+
+		// 自分を登録している空間に自身を通知
+		if (!p_Space->OnRemove(this)) { return false; }
+
+		// 逸脱処理
+		// 前後のオブジェクトを結びつける
+		if (sp_Pre)
+		{
+			sp_Pre->sp_Next = sp_Next;
+			sp_Pre.RegistPtr(nullptr);
+		}
+		if (sp_Next)
+		{
+			sp_Next->sp_Pre = sp_Pre;
+			sp_Next.reset();
+		}
+
+		p_Space = nullptr;
+		return true;
+	}
+
+
+	// 空間を登録
+	template<class T>
+	inline void QuadTreeObject<T>::RegisterSpace(QuadTreeSpace<T>* pSpace)
+	{
+		p_Space = pSpace;
+	}
+
+
+	// 次のオブジェクトを取得
+	template<class T>
+	inline std::shared_ptr<QuadTreeObject<T>> QuadTreeObject<T>::GetNextObj() const
+	{
+		return sp_Next;
+	}
 }
 

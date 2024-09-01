64c78d8b7254c730feea6aa81ae21490d6c55966
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-09-01 13:14:32 +0900

    サンプルカラー構造体を作った

---
+++ Project/Math/MyColor/MyColor.cpp
@@ -0,0 +1,15 @@
+#include "MyColor.h"
+
+
+
+/// <summary>
+/// サンプルカラー
+/// </summary>
+const Vector4 Smp::Color::RED(1.0f, 0.0f, 0.0f, 1.0f);
+const Vector4 Smp::Color::GREEN(0.0f, 1.0f, 0.0f, 1.0f);
+const Vector4 Smp::Color::BLUE(0.0f, 0.0f, 1.0f, 1.0f);
+const Vector4 Smp::Color::YELLOW(1.0f, 1.0f, 0.0f, 1.0f);
+const Vector4 Smp::Color::CYAN(0.0f, 1.0f, 1.0f, 1.0f);
+const Vector4 Smp::Color::MAGENTA(1.0f, 0.0f, 1.0f, 1.0f);
+const Vector4 Smp::Color::WHITE(1.0f, 1.0f, 1.0f, 1.0f);
+const Vector4 Smp::Color::BLACK(0.0f, 0.0f, 0.0f, 1.0f);

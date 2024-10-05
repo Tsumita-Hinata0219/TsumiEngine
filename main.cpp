a3bf742ae3ad7a2acace0aa420238015b6acb928
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-10-05 20:53:11 +0900

    とりあえずちゃんと歪んだ

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	std::unique_ptr<GameManager> gameManager = 
-		std::make_unique<GameManager>(new GameScene);
+		std::make_unique<GameManager>(new DebugScene);
 	gameManager->Run();
 	return 0;
 }

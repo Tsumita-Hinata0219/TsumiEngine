eeba3d405800a5d30c195dd2e5579c94ea14dd73
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-10-07 15:21:01 +0900

    ラベルを出すクラス作成

---
+++ Project/Application/GameObject/Others/TitleLabelUI/TitleLabelUI.cpp
@@ -0,0 +1,20 @@
+#include "TitleLabelUI.h"
+
+
+
+// 初期化処理
+void TitleLabelUI::Init()
+{
+}
+
+
+// 更新処理
+void TitleLabelUI::Update()
+{
+}
+
+
+// 描画処理
+void TitleLabelUI::Draw2DFront()
+{
+}
---
+++ Project/Application/GameObject/Others/TitleLabelUI/TitleLabelUI.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "GameObject/GameObject.h"
+
+
+// Labelの種類
+enum class TitleLabelType {
+	Start,
+	Exsit,
+	Count,
+};
+
+/* TitleLabelUIクラス */
+class TitleLabelUI {
+
+public:
+
+	// コンストラクタ、デストラクタ
+	TitleLabelUI() = default;
+	~TitleLabelUI() = default;
+
+	// 初期化、更新、描画
+	void Init();
+	void Update();
+	void Draw2DFront();
+
+
+private:
+
+	// ラベル
+	std::vector<SpriteProperty> label_;
+
+};
+

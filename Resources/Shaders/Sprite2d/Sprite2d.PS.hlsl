a7d9e7ffcb398fd459fb2536bc54198b005f898c
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-10-09 16:40:09 +0900

    save

--- Resources/Shaders/Sprite2d/Sprite2d.PS.hlsl
+++ Resources/Shaders/Sprite2d/Sprite2d.PS.hlsl
@@ -22,7 +22,7 @@ PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
     
-    float4 transformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    float4 transformedUV = mul(float4(input.texCoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
 
     output.color.rgb = gMaterial.color.rgb * textureColor.rgb;

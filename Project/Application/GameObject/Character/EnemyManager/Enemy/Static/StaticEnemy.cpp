4596854fc8b9f217d7d09877e69a142dbf482536
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-12 23:21:53 +0900

    EnemyBulletをEnemyManagerに持たせた

--- Project/Application/GameObject/Character/EnemyManager/Enemy/Static/StaticEnemy.cpp
+++ Project/Application/GameObject/Character/EnemyManager/Enemy/Static/StaticEnemy.cpp
@@ -120,23 +120,31 @@ void StaticEnemy::ExecutexShot()
 // 新しいバレットを生成する
 void StaticEnemy::CreateNewBullet()
 {
-	// newBulletのインスタンス
-	std::shared_ptr<EnemyBullet> newBullet = std::make_unique<EnemyBullet>();
+	//// newBulletのインスタンス
+	//std::shared_ptr<EnemyBullet> newBullet = std::make_unique<EnemyBullet>();
+
+	//// 初期座標
+	//Vector3 initPos = trans_.GetWorldPos();
+	//// 初期速度
+	//Vector3 initVel = Vector3::oneZ;
+	//initVel.z = 0.2f;
+	//initVel = TransformNormal(initVel, trans_.matWorld);
+
+	//// newBulletの初期化
+	//newBullet->Init();
+	//newBullet->SetPosition(initPos);
+	//newBullet->SetVelocity(initVel);
+	//newBullet->SetRotationFromVelocity();
+
+	//// リストに追加
+	//bulletList_.push_back(newBullet);
 
 	// 初期座標
 	Vector3 initPos = trans_.GetWorldPos();
 	// 初期速度
 	Vector3 initVel = Vector3::oneZ;
-	initVel.z = 0.2f;
+	initVel.z = 0.3f;
 	initVel = TransformNormal(initVel, trans_.matWorld);
-
-	// newBulletの初期化
-	newBullet->Init();
-	newBullet->SetPosition(initPos);
-	newBullet->SetVelocity(initVel);
-	newBullet->SetRotationFromVelocity();
-
-	// リストに追加
-	bulletList_.push_back(newBullet);
+	enemyManager_->AddNewEnemyBullet(initPos, initVel);
 }
 

b467b1415279356832a71922256f7f106b538612
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-14 16:16:23 +0900

    プレイヤーの移動処理のステートパターンの基底クラスを作成

--- Project/Application/GameObject/Character/Player/State/Movement/IPlayerMovement.h
+++ Project/Application/GameObject/Character/Player/State/Movement/IPlayerMovement.h
@@ -1,1 +1,22 @@
 #pragma once
+
+
+// Playerの前方宣言
+class Player;
+
+/* IPlayerMovementの基底クラス
+   移動処理のステートパターン */
+class IPlayerMovement {
+
+public:
+
+	// 仮想デストラクタ
+	virtual ~IPlayerMovement() = default;
+
+	// 初期化、更新
+	virtual void Eneter() = 0;
+	virtual void Update() = 0;
+
+
+};
+

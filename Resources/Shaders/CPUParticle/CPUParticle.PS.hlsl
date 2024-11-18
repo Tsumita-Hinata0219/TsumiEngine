a9cc025b2e95d5fcb3d9109ab98544a3a23b0630
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-18 21:50:42 +0900

    シェーダーを新しく作り直す

---
+++ Resources/Shaders/CPUParticle/CPUParticle.PS.hlsl
@@ -0,0 +1,49 @@
+#include "CPUParticle.hlsli"
+
+// マテリアル
+struct Material
+{
+    float4 color;
+    float4x4 uvTransform;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+Texture2D<float4> gTexture : register(t0);
+
+// カメラ
+struct ViewProjectionMatrix
+{
+    float4x4 view;
+    float4x4 projection;
+    float3 cameraPosition;
+};
+ConstantBuffer<ViewProjectionMatrix> gViewProjectionMat : register(b1);
+
+SamplerState gSampler : register(s0);
+
+
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+
+    // UV変換を適用
+    float4 transformedUV = mul(float4(input.texCoord, 0.0f, 1.0f), gMaterial.uvTransform);
+
+    // テクスチャから色をサンプリング
+    float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+
+    // アルファ値が0.9f未満なら描画をスキップ
+    if (textureColor.a < 0.9f)
+    {
+        discard;
+    }
+    
+    // アルファ値の計算
+    output.color.a = gMaterial.color.a * textureColor.a;
+
+    return output;
+}
---
+++ Resources/Shaders/CPUParticle/CPUParticle.VS.hlsl
@@ -0,0 +1,38 @@
+#include "CPUParticle.hlsli"
+
+struct TransformationMatrix
+{
+    float4x4 World;
+    float4x4 WVP;
+    float4x4 WorldInverseTranspose;
+};
+StructuredBuffer<TransformationMatrix> gTransformationMat : register(t0);
+
+
+struct VertexShaderInput
+{
+    float4 position : POSITION0;
+    float2 texCoord : TEXCOORD0;
+    float3 normal : NORMAL0;
+    float3 worldPos : WORLDPOSITION0;
+};
+
+
+VertexShaderOutput main(VertexShaderInput input, int instanceID : SV_InstanceID)
+{
+    VertexShaderOutput output;
+    
+    // 頂点の位置をワールド・ビュー・プロジェクション行列で変換
+    output.position = mul(input.position, gTransformationMat[instanceID].WVP);
+    
+    // テクスチャ座標はそのまま渡す
+    output.texCoord = input.texCoord;
+    
+    // 法線ベクトルをワールド行列の逆転置行列で変換し、正規化する
+    output.normal = normalize(mul(input.normal, (float3x3) gTransformationMat[instanceID].WorldInverseTranspose));
+    
+    // ワールド空間での位置を計算して出力に設定
+    output.worldPos = mul(input.position, gTransformationMat[instanceID].World).xyz;
+    
+    return output;
+}

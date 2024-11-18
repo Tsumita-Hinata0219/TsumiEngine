8f02cd7217ec24a01e51af9ef28a8abbc81bf9f0
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-18 14:35:39 +0900

    save

--- Project/Application/GameObject/Others/StageTransitionMenu/NaviBack/StageTransitionMenuNaviBack.cpp
+++ Project/Application/GameObject/Others/StageTransitionMenu/NaviBack/StageTransitionMenuNaviBack.cpp
@@ -1,1 +1,34 @@
 #include "StageTransitionMenuNaviBack.h"
+
+
+
+/// <summary>
+/// 初期化処理
+/// </summary>
+void StageTransitionMenuNaviBack::Init()
+{
+}
+
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void StageTransitionMenuNaviBack::Update()
+{
+}
+
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void StageTransitionMenuNaviBack::Draw2DFront()
+{
+}
+
+
+/// <summary>
+/// ImGuiの描画
+/// </summary>
+void StageTransitionMenuNaviBack::DrawImGui()
+{
+}
--- Project/Application/GameObject/Others/StageTransitionMenu/NaviBack/StageTransitionMenuNaviBack.h
+++ Project/Application/GameObject/Others/StageTransitionMenu/NaviBack/StageTransitionMenuNaviBack.h
@@ -1,5 +1,68 @@
 #pragma once
-class StageTransitionMenuNaviBack
-{
+
+#include <list>
+#include <memory>
+
+#include "../../../IObject/IObject.h"
+#include "../../../GameObject.h"
+#include "../IStageTransitionMenu.h"
+#include "PostEffect/IPostEffect/RadialBlur/RadialBlurEffect.h"
+
+
+/* ゲーム終了メニューのナビゲーションバック */
+class StageTransitionMenuNaviBack {
+
+public:
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	StageTransitionMenuNaviBack() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~StageTransitionMenuNaviBack() = default;
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Init();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw2DFront();
+
+private:
+
+	/// <summary>
+	/// ImGuiの描画
+	/// </summary>
+	void DrawImGui();
+
+
+private:
+
+	// テクスチャハンドル
+	uint32_t backTexHD_ = 0;
+	uint32_t frameTexHD_ = 0;
+
+	// スプライト
+	std::unique_ptr<Sprite> back_;
+	std::unique_ptr<Sprite> frame_;
+
+	// トランスフォーム
+	Transform backTrans_{};
+	Transform frameTrans_{};
+
+	// カラー
+	Vector4 color_{};
+
 };
 

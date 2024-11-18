5f1b76912f9f8778beffe32006ade2b04c116163
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-18 22:57:04 +0900

    描画していく

--- Project/Engine/Particle/BaseParticle/BaseParticle.cpp
+++ Project/Engine/Particle/BaseParticle/BaseParticle.cpp
@@ -1,1 +1,26 @@
 #include "BaseParticle.h"
+
+
+
+/// <summary>
+/// 初期化
+/// </summary>
+void BaseParticle::Init()
+{
+}
+
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void BaseParticle::Update()
+{
+}
+
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void BaseParticle::Draw3D()
+{
+}
--- Project/Engine/Particle/BaseParticle/BaseParticle.h
+++ Project/Engine/Particle/BaseParticle/BaseParticle.h
@@ -1,3 +1,49 @@
 #pragma once
 
-#include "Particle/IParticle.h"
+#include "GameObject/GameObject.h"
+
+
+/* ベースパーティクルクラス */
+class BaseParticle {
+
+public:
+
+	/// <summary>
+	/// コンストラクト
+	/// </summary>
+	BaseParticle() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~BaseParticle() = default;
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Init();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw3D();
+
+
+private:
+
+	// CameraManager
+	CameraManager* cameraManager_ = nullptr;
+	
+	// モデル
+	std::unique_ptr<Model> model_;
+
+	// トランスフォーム
+	Transform trans_{};
+
+};
+

d5ba9567e40e7809fee3eb938c1abe080c84130d
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-18 21:38:57 +0900

    save

--- Project/Application/Scene/DebugScene/DebugScene.cpp
+++ Project/Application/Scene/DebugScene/DebugScene.cpp
@@ -19,35 +19,19 @@ DebugScene::~DebugScene() {}
 /// </summary>
 void DebugScene::Initialize()
 {
-	/* ----- ConfigManager コンフィグマネージャー ----- */
-	configManager_ = ConfigManager::GetInstance();
-	configManager_->LoadFile();
-
 	/* ----- Camera カメラ ----- */
 	camera_.Init({ 0.2f, 0.0f, 0.0f }, { 0.0f, 1.7f, -4.0f });
 	cameraManager_ = CameraManager::GetInstance();
 	cameraManager_->ReSetData(camera_);
 	
-	/* ----- Skydome 天球 ----- */
-	skydome_ = std::make_unique<Skydome>();
-	skydome_->Init();
-
 	/* ----- Skybox 天箱 ----- */
 	skybox_ = std::make_unique<Skybox>();
 	uint32_t dds = TextureManager::LoadTexture("Texture", "DemoSkybox.dds");
 	skybox_->Init(dds);
 
-	/* ----- Ground 床 ----- */
-	ground_ = std::make_unique<Ground>();
-	ground_->Init();
-
 	/* ----- TestPostEffect テストポストエフェクト ----- */
 	testPostEffect_ = make_unique<TestPostEffect>();
 	testPostEffect_->Init();
-
-	/* ----- TestHuman テストヒューマン ----- */
-	testHuman_ = make_unique<TestHuman>();
-	testHuman_->Init();
 }
 
 
@@ -62,30 +46,16 @@ void DebugScene::Update(GameManager* state)
 	/* ----- Camera カメラ ----- */
 	camera_.Update();
 
-	/* ----- Skydome 天球 ----- */
-	skydome_->Update();
-
 	/* ----- Skybox 天箱 ----- */
 	skybox_->Update();
-
-	/* ----- Ground 床 ----- */
-	ground_->Update();
 
 	/* ----- TestPostEffect テストポストエフェクト ----- */
 	testPostEffect_->Update();
 
-	/* ----- TestHuman テストヒューマン ----- */
-	testHuman_->Update();
-
 #ifdef _DEBUG
-
 	ImGui::Begin("DebugScene");
 	camera_.DrawImGui();
 	ImGui::End();
-
-	/* ----- ConfigManager コンフィグマネージャー ----- */
-	configManager_->DrawImGui();
-
 #endif // _DEBUG
 }
 
@@ -104,18 +74,8 @@ void DebugScene::BackSpriteDraw()
 /// </summary>
 void DebugScene::ModelDraw()
 {
-	/* ----- Skydome 天球 ----- */
-	//skydome_->Draw3D();
-
 	/* ----- Skybox 天箱 ----- */
 	skybox_->Draw();
-
-	/* ----- Ground 床 ----- */
-	//ground_->Draw3D();
-
-	/* ----- TestHuman テストヒューマン ----- */
-	testHuman_->Draw();
-
 }
 
 

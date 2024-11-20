e2150edf0f435ae1ab1f26af506a65c2bb646e96
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-20 14:44:00 +0900

    Viewの扱いをもっと簡単にするためにDescriptorManagerをつくる

--- Project/Engine/GPUParticle/Particle/GPUParticle.cpp
+++ Project/Engine/GPUParticle/Particle/GPUParticle.cpp
@@ -50,12 +50,36 @@ void GPUParticle::Draw()
 /// </summary>
 void GPUParticle::CommandCallInit()
 {
+
 }
 void GPUParticle::CommandCallUpdate()
 {
+
 }
 void GPUParticle::CommandCallDraw()
 {
+	// Commandの取得
+	Commands commands = CommandManager::GetInstance()->GetCommands();
+
+	// PipeLineCheck
+	PipeLineManager::PipeLineCheckAndSet(PipeLineType::Object3D);
+
+	// VertexBufferView
+	buffers_.vertex.IASetVertexBuffers(1);
+	// IndexBufferView
+	buffers_.indeces.IASetIndexBuffer();
+	// Material
+	buffers_.material.CommandCall(0);
+	// Transform
+	buffers_.transform.CommandCall(1);
+	// Camera
+	cameraManager_->CommandCall(2);
+	// MaterialTexture
+	SRVManager::SetGraphicsRootDescriptorTable(3, datas_.material.textureHandle);
+	// Light
+	//buffers_.light.CommandCall(4);
+	// Draw!!
+	commands.List->DrawInstanced(6, instanceNum_, 0, 0);
 }
 
 
@@ -64,5 +88,19 @@ void GPUParticle::CommandCallDraw()
 /// </summary>
 void GPUParticle::CreateBufferResource()
 {
+	// mesh
+	buffers_.mesh.CreateResource();
+	// vertexBufferView
+	buffers_.vertex.CreateResource();
+	buffers_.vertex.CreateVertexBufferView();
+	// indexBufferView
+	buffers_.indeces.CreateResource();
+	buffers_.indeces.CreateIndexBufferView();
+	// material
+	buffers_.material.CreateResource();
+	// transform
+	buffers_.transform.CreateResource(instanceNum_); // インスタンス数分つくる
+	// light
+	//buffers_.light.CreateResource();
 }
 

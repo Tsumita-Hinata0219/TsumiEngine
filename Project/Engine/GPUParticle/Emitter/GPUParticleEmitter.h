c226c5c1d88e903c97fd10e2f48127474284a67b
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-24 20:24:38 +0900

    Emiterクラス用意

--- Project/Engine/GPUParticle/Emitter/GPUParticleEmitter.h
+++ Project/Engine/GPUParticle/Emitter/GPUParticleEmitter.h
@@ -1,5 +1,44 @@
 #pragma once
-class GPUParticleEmitter
-{
+
+#include "GameObject/GameObject.h"
+#include "../Structure/GPUParticleEmitterStructure.h"
+#include "BufferResource/BufferResource.h"
+
+
+/* GPUParticleのEmitterクラス */
+class GPUParticleEmitter {
+
+public: 
+	
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	GPUParticleEmitter() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GPUParticleEmitter() = default;
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Init();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+
+private:
+
+
+
 };
 

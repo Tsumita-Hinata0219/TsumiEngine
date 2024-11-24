73cd4aa69db3945c6be16f572523f523a1284276
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-24 23:41:49 +0900

    save

--- Project/Engine/GPUParticle/Structure/GPUParticleEmitterStructure.h
+++ Project/Engine/GPUParticle/Structure/GPUParticleEmitterStructure.h
@@ -3,17 +3,18 @@
 #include "Math/MyMath.h"
 
 // GPUParticleのエミッター
-namespace GParticleEmitte {
+namespace GPUParticle {
+	namespace Emit {
 
-	// Sphere型のEmitter
-	struct SphereEmitter {
-		Vector3 translate{}; // 座標
-		float radius = 0.0f; // 射出半径
-		uint32_t count = 0; // 射出数
-		float frequency = 0.0f; // 射出間隔
-		float frequencyTime = 0.0f; // 射出間隔調整用時間
-		uint32_t emit = 0.0f; // 射出許可
-	};
-
+		// Sphere型のEmitter
+		struct SphereEmitter {
+			Vector3 translate{}; // 座標
+			float radius = 0.0f; // 射出半径
+			uint32_t count = 0; // 射出数
+			float frequency = 0.0f; // 射出間隔
+			float frequencyTime = 0.0f; // 射出間隔調整用時間
+			uint32_t emit = 0; // 射出許可
+		};
+	}
 }
 

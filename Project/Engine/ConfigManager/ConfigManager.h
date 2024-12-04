874e7ee5aded511d6d2d5e2af47e617fd9cf2426
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-12-04 14:09:27 +0900

    cinfigManagerのgetGrop削除

--- Project/Engine/ConfigManager/ConfigManager.h
+++ Project/Engine/ConfigManager/ConfigManager.h
@@ -92,20 +92,6 @@ public: // メンバ関数
 
 #pragma region Accessor アクセッサ
 
-	// グループの取得
-	ConfigGroup& GetGroup(const std::string& groupName) {
-		// 指定された名前で検索をかける
-		auto it = datas_.find(groupName);
-
-		// ヒットしたらそのグループをreturn
-		if (it != datas_.end()) {
-			return it->second;
-		}
-
-		// 見つからなかった場合のエラーハンドリング
-		Log("\n指定されたグループは存在しません\n新しくグループを作成するか、指定名を修正してください\n");
-		throw std::runtime_error("Group not found: " + groupName);
-	}
 
 #pragma endregion 
 

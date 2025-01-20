ebbc7bf5c9d7670cc26a93eda72928c88dfd5093
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-01-20 15:29:24 +0900

    save

--- Project/Application/GameObject/Character/Player/OperationUI/Interface/IOperationUI.cpp
+++ Project/Application/GameObject/Character/Player/OperationUI/Interface/IOperationUI.cpp
@@ -1,20 +1,23 @@
 #include "IOperationUI.h"
 
-
-
 /// <summary>
 /// 初期化処理
 /// </summary>
 void IOperationUI::Init() 
 {
+    // スプライトの初期化
+    sprite_ = std::make_unique<Sprite>();
+    sprite_->SetTexture(textureHandle_);
 
+    // トランスフォームの初期化
+    trans_.Init();
 }
 
 
 /// <summary>
 /// 更新処理
 /// </summary>
-void IOperationUI::Update()
+void IOperationUI::Update() 
 {
 
 
@@ -25,8 +28,6 @@ void IOperationUI::Update()
 /// 描画処理
 /// </summary>
 void IOperationUI::Draw2DFront() 
-{
-
-
-
+{ 
+    sprite_->Draw(trans_); 
 }

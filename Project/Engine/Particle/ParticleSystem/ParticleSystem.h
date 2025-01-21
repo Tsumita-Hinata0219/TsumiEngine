9aebfa466c7cb22d9e8a338f406b4ebc537d73ae
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-01-21 15:43:25 +0900

    save

--- Project/Engine/Particle/ParticleSystem/ParticleSystem.h
+++ Project/Engine/Particle/ParticleSystem/ParticleSystem.h
@@ -8,6 +8,5 @@
 #include "View/SRVManager/SRVManager.h"
 #include "Math/MyMath.h"
 #include "Math/Struct.h"
-#include "GameObject/Camera/Camera.h"
 #include "GameObject/Camera/Manager/CameraManager.h"
 #define INSTANCE_MAX 1000;

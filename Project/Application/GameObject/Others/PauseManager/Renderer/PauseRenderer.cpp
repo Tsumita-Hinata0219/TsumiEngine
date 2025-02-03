e7fedffbfa72cf475cf146f08d4963c420e33b4f
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-02-03 20:13:50 +0900

    save

---
+++ Project/Application/GameObject/Others/PauseManager/Renderer/PauseRenderer.cpp
@@ -0,0 +1,18 @@
+#include "PauseRenderer.h"
+
+
+
+/// <summary>
+/// 初期化処理
+/// </summary>
+void PuaseRenderer::Init()
+{
+}
+
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void PuaseRenderer::Update()
+{
+}
--- Project/Application/GameObject/Others/PauseManager/Renderer/PauseRenderer.h
+++ Project/Application/GameObject/Others/PauseManager/Renderer/PauseRenderer.h
@@ -1,1 +1,31 @@
 #pragma once
+
+
+/* ポーズ時の描画クラス */
+class PuaseRenderer {
+
+public:
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	PuaseRenderer() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~PuaseRenderer() = default;
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Init();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+private:
+
+};

f7954081693067841e448bebdc1c46e7c7b70beb
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-02-21 12:22:56 +0900

    save

--- Project/Engine/FlagManager/FlagManager.cpp
+++ Project/Engine/FlagManager/FlagManager.cpp
@@ -1,19 +1,2 @@
 #include "FlagManager.h"
 
-
-
-/// <summary>
-/// コンストラクタ
-/// </summary>
-FlagManager::FlagManager()
-{
-}
-
-
-/// <summary>
-/// JSONファイルからフラグ定義をロードする。
-/// </summary>
-void FlagManager::LoadFlagsFromFile(const std::string& filename)
-{
-}
-

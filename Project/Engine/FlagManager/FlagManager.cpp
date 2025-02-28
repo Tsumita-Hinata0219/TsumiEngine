ed77ce5fdce304b2eb75c01487e4d8d39c129f20
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-02-28 11:14:35 +0900

    save

--- Project/Engine/FlagManager/FlagManager.cpp
+++ Project/Engine/FlagManager/FlagManager.cpp
@@ -1,2 +1,44 @@
 #include "FlagManager.h"
 
+
+
+/// <summary>
+/// JSONファイルからフラグをロード
+/// </summary>
+void FlagManager::LoadFlagFromJson(const std::string& rootPath, const std::string& fileName)
+{
+}
+
+
+/// <summary>
+/// フラグの取得
+/// </summary>
+bool FlagManager::GetFlag(const std::string& key) const
+{
+	return false;
+}
+
+
+/// <summary>
+/// フラグの設定
+/// </summary>
+void FlagManager::SetFlag(const std::string& key, bool value)
+{
+}
+
+
+/// <summary>
+/// 全フラグをリセット
+/// </summary>
+void FlagManager::ClearFlag(const std::string& key)
+{
+}
+
+
+/// <summary>
+/// JSONファイルにフラグを保存
+/// </summary>
+bool FlagManager::SaveFlagToJson(const std::string& rootPath, const std::string& fileName)
+{
+	return false;
+}

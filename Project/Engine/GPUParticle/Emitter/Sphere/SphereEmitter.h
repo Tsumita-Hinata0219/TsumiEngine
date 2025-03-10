061afa784d2830f099fcc0f7b1a866b0bfd53e1c
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-03-10 12:24:46 +0900

    Emitterの派生クラス作成

---
+++ Project/Engine/GPUParticle/Emitter/Sphere/SphereEmitter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../Interface/IEmitter.h"
+
+
+class SphereEmitter : public IEmitter<Emitter::SphereEmit> {
+
+public:
+
+	/// <summary>
+	/// コンストラク
+	/// </summary>
+	SphereEmitter() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~SphereEmitter() = default;
+
+
+private:
+
+};

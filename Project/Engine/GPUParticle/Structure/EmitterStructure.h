244563ad0f4b6c50ffa4954b66d9d5109e79ffd0
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-03-10 12:18:32 +0900

    Emitterの基底クラス作った

--- Project/Engine/GPUParticle/Structure/EmitterStructure.h
+++ Project/Engine/GPUParticle/Structure/EmitterStructure.h
@@ -53,3 +53,56 @@ struct RandomSeed {
 };
 
 }
+
+namespace Emitter {
+
+// Emitterの基底構造体
+struct IEmitData {
+    Vector3 translate{}; // 座標
+};
+
+// Sphere 派生構造体
+struct SphereEmit : IEmitData {
+    float radius = { 0 }; // 半径
+};
+
+// Box 派生構造体
+struct BoxEmit : IEmitData {
+    Vector3 min{}; // 
+    Vector3 max{}; // 
+};
+
+// 範囲
+struct EmitRange {
+    // SR
+    Vector4 scaleMin;
+    Vector4 scaleMax;
+    Vector4 rotateMin;
+    Vector4 rotateMax;
+    // Color
+    Vector4 colorMin;
+    Vector4 colorMax;
+    // Motion
+    Vector4 velocityMin;
+    Vector4 velocityMax;
+    // Life
+    float baseLifeTime = 0.0f;
+    float lifeTimeMin = 0.0f;
+    float lifeTimeMax = 0.0f;
+};
+
+// 射出
+struct EmitConfig {
+    uint32_t spawnCount; // 生成数
+    float spawnInterval = 0.0f; // インターバル
+    float elapsedTime = 0.0f; // 射出タイム
+    uint32_t isEmitting; // 射出許可フラグ (0: 停止, 1: 許可)
+};
+
+// 乱数のシード情報
+struct RandomSeed {
+    float gameTime = 0.0f;
+    float dynamicTime = 0.0f;
+};
+
+}

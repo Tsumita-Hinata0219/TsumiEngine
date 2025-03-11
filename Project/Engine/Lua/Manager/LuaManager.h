9ed2ae35a00c94c7c0d7c4f54c51c39689ada266
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-03-11 20:40:19 +0900

    save

---
+++ Project/Engine/Lua/Manager/LuaManager.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Math/MyMath.h"
+
+#include "../Script/LuaScript.h"
+
+
+/* Luaスクリプトの管理を一元化するクラス */
+class LuaManager {
+
+public:
+
+	/// <summary>
+	/// コンストラク
+	/// </summary>
+	LuaManager();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~LuaManager();
+
+	/// <summary>
+	/// 指定したLuaスクリプトの読み込み
+	/// </summary>
+	std::shared_ptr<LuaScript> LoadScript(const std::string& scriptName, const std::string& fileName);
+
+	/// <summary>
+	/// 指定したスクリプトを取得
+	/// </summary>
+	std::shared_ptr<LuaScript> GetScript(const std::string& scriptName);
+
+	/// <summary>
+	/// 指定したスクリプトを解放
+	/// </summary>
+	void UnLoadScript(const std::string& scriptName);
+
+
+private:
+
+	std::unordered_map<std::string, std::shared_ptr<LuaScript>> scripts_;
+
+};
---
+++ Project/Engine/Lua/Script/LuaScript.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iostream>
+#include <lua.hpp>
+
+#include "Math/MyMath.h"
+
+
+/* 個々のスクリプトを管理するクラス */
+class LuaScript {
+
+public:
+
+	/// <summary>
+	/// コンストラク
+	/// </summary>
+	LuaScript();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~LuaScript();
+
+	/// <summary>
+	/// スクリプトの読み込み
+	/// </summary>
+	bool LoadScript(const string& fileName);
+
+	/// <summary>
+	/// Lua側の関数を実行
+	/// </summary>
+	bool ExeFunction(const string& funcName, int arg = 0);
+
+	/// <summary>
+	/// Lua側の数値変数を取得
+	/// </summary>
+	float GetNumber(const std::string& verName);
+
+	/// <summary>
+	/// Lua側の文字列変数を取得
+	/// </summary>
+	std::string GetString(const std::string& verName);
+
+	/// <summary>
+	/// Luaステートを閉じる(メモリ解放)
+	/// </summary>
+	void Close();
+
+
+private:
+
+};

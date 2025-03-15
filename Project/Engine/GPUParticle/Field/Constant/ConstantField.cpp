cd9342309e597f7342e5fa3b9afc2c8b1e8ee98c
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-03-16 00:42:06 +0900

    save

--- Project/Engine/GPUParticle/Field/Constant/ConstantField.cpp
+++ Project/Engine/GPUParticle/Field/Constant/ConstantField.cpp
@@ -7,7 +7,15 @@
 /// </summary>
 GpuField::Data::ConstantField GpuField::ConstantField::LoadFieldData(const std::weak_ptr<LuaScript>& lScript)
 {
-    lScript;
+    GpuField::Data::ConstantField result;
 
-    return GpuField::Data::ConstantField();
+    result.acceleration = lScript.lock()->GetVariable<Vector3>("");
+    result.damping = lScript.lock()->GetVariable<float>("");
+    result.angularVelocity = lScript.lock()->GetVariable<Vector3>("");
+    result.drag = lScript.lock()->GetVariable<float>("");
+    result.force = lScript.lock()->GetVariable<Vector3>("");
+    result.mass = lScript.lock()->GetVariable<float>("");
+    result.isUse = lScript.lock()->GetVariable<int>("");
+
+    return result
 }

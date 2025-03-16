483d9552771cbf94104f36d17b510279d7d1e9d6
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-03-16 22:43:34 +0900

    明らかにEmitterの位置と実際の出現位置があってない

--- Project/Application/GameObject/Dev/DummyParticle/DummyParticle.h
+++ Project/Application/GameObject/Dev/DummyParticle/DummyParticle.h
@@ -14,6 +14,10 @@ private:
 	std::weak_ptr<LuaScript> lua_;
 	LuaManager* luaManager_ = nullptr;
 
+	std::unique_ptr<Model> model_;
+	ModelManager* modelManager_ = nullptr;
+	Transform trans_{};
+
 public:
 
 	DummyParticle() = default;
@@ -35,6 +39,13 @@ public:
 		sEmit_->Load_EmitRangeData_From_Lua(lua_);
 		sEmit_->Load_EmitConfigData_From_Lua(lua_);
 		cField_->Load_FieldData_From_Lua(lua_);
+
+		modelManager_ = ModelManager::GetInstance();
+		modelManager_->LoadModel("Obj/Emitter/Sphere", "SphereEmitter.obj");
+		model_ = modelManager_->GetModel("SphereEmitter");
+		model_->SetRenderState(RenderState::FillMode_WireFrame);
+
+		trans_.Init();
 	}
 
 	void Update()
@@ -44,16 +55,23 @@ public:
 
 		if (auto lockedData = sEmit_->GetWeak_EmitData().lock()) {
 
-			Vector4 vel = { 0.0f, 0.0f, 0.5f, 0.0f };
+			Vector4 vel = { 0.0f, 0.0f, 0.1f, 0.0f };
 			if (lua_.lock()->ExeFunction("Move", vel)) {
 				lockedData->translate = lua_.lock()->GetVariable<Vector4>("position");
 			}
+
+			trans_.srt.translate = {
+				lockedData->translate.x,
+				lockedData->translate.y,
+				lockedData->translate.z,
+			};
 		}
 	}
 
 	void Draw()
 	{
 		sEmit_->Draw3D();
+		model_->Draw(trans_);
 	}
 
 private:

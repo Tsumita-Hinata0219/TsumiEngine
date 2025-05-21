382724feec6dd11910730850a9f50c0ae77d55c6
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-05-21 11:13:46 +0900

    ちゃんと動いた

--- Project/Engine/GPUParticle/Field/Constant/ConstantField.cpp
+++ Project/Engine/GPUParticle/Field/Constant/ConstantField.cpp
@@ -9,13 +9,13 @@ void GpuField::ConstantField::Load_FieldData_From_Lua(const LuaScript& lua)
 {
 	GpuField::Data::ConstantField result;
 
-	result.acceleration = lua.GetVariable<Vector3>("");
-	result.damping = lua.GetVariable<float>("");
-	result.angularVelocity = lua.GetVariable<Vector3>("");
-	result.drag = lua.GetVariable<float>("");
-	result.force = lua.GetVariable<Vector3>("");
-	result.mass = lua.GetVariable<float>("");
-	result.isUse = lua.GetVariable<int>("");
+	result.acceleration = lua.GetVariable<Vector3>("EmitConfig.acceleration");
+	result.damping = lua.GetVariable<float>("damping");
+	result.angularVelocity = lua.GetVariable<Vector3>("EmitConfig.angularVelocity");
+	result.drag = lua.GetVariable<float>("EmitConfig.drag");
+	result.force = lua.GetVariable<Vector3>("EmitConfig.force");
+	result.mass = lua.GetVariable<float>("EmitConfig.mass");
+	result.isUse = lua.GetVariable<bool>("EmitConfig.isUse");
 
 	*fieldData_ = result;
 }
